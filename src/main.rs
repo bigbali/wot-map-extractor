af5c8f0de76c74ecff3e775784e2d10145f8bc01
Balázs Burján <bigbali141@gmail.com>
2023-09-09 22:34:28 +0300

    section table hashmap

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,94 @@ use std::{
     fs::File,
     io::Read,
     io::{Error, ErrorKind},
+    mem,
+    ops::{Deref, DerefMut},
 };
 
 const HEADER_ENTRIES: usize = 29;
 
-#[derive(Default, Copy, Clone, Debug)]
+type EntryValue = [i32; 5];
+
+#[derive(Default, Clone, Debug)]
 struct Entry {
-    descriptor: [u8; 4],
-    body: [i32; 5],
+    name: String,
+    description: Option<String>,
+    value: EntryValue,
+}
+
+impl Entry {
+    fn set_value(&mut self, value: EntryValue) -> &Self {
+        let _ = mem::replace(&mut self.value, value);
+        self
+    }
 }
 
 #[derive(Default, Debug)]
-struct DataTable {
-    raw_entries: [Entry; HEADER_ENTRIES],
-    table: HashMap<String, [i32; 5]>,
+struct SectionTable(HashMap<String, Entry>);
+
+fn section(key: &str, description: &str) -> (String, Entry) {
+    let name = key.to_string();
+
+    (
+        name.clone(),
+        Entry {
+            name,
+            description: Some(description.to_string()),
+            ..Default::default()
+        },
+    )
 }
 
-impl DataTable {
-    fn new() -> DataTable {
-        DataTable {
-            raw_entries: Default::default(),
-            table: HashMap::with_capacity(HEADER_ENTRIES),
-        }
+impl SectionTable {
+    fn from_hashmap(map: HashMap<String, Entry>) -> Self {
+        Self(map)
+    }
+
+    fn new() -> Self {
+        Self::from_hashmap(HashMap::from([
+            section("BWTB", "unknown"),
+            section("BWST", "unknown"),
+            section("BWAL", "unknown"),
+            section("BWCS", "unknown"),
+            section("BWSG", "unknown"),
+            section("BSGD", "unknown"),
+            section("BWS2", "unknown"),
+            section("BSG2", "unknown"),
+            section("BWT2", "unknown"),
+            section("BSMI", "unknown"),
+            section("BSMO", "unknown"),
+            section("BSMA", "unknown"),
+            section("SpTr", "unknown"),
+            section("WGSD", "unknown"),
+            section("WTCP", "unknown"),
+            section("BWWa", "unknown"),
+            section("BWEP", "unknown"),
+            section("WGCO", "unknown"),
+            section("BWPs", "unknown"),
+            section("CENT", "Content"),
+            section("UDOS", "unknown"),
+            section("WGDE", "unknown"),
+            section("BWLC", "unknown"),
+            section("BWVL", "unknown"),
+            section("WTau", "unknown"),
+            section("WTbl", "unknown"),
+            section("WGSH", "unknown"),
+            section("WGMM", "unknown"),
+            section("GOBJ", "unknown"),
+        ]))
+    }
+}
+
+impl Deref for SectionTable {
+    type Target = HashMap<String, Entry>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl DerefMut for SectionTable {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
 
@@ -37,7 +103,6 @@ fn error(e: Error, m: &str, path: Option<&String>) -> String {
 }
 
 fn main() {
-    // set_hook(Box::new(error));
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
 
@@ -59,13 +124,13 @@ fn parse_binary(buffer: Vec<u8>, path: &String) {
     let table = parse_header(buffer)
         .unwrap_or_else(|e| panic!("{}", error(e, "Failed to parse header", Some(path))));
 
-    for (k, v) in table.table.into_iter() {
-        println!("{:?}{:?}", k, v);
+    for (k, v) in table.clone().into_iter() {
+        println!("{:?} {:?}", k, v);
     }
 }
 
-fn parse_header(buffer: Vec<u8>) -> Result<DataTable, Error> {
-    let mut table = DataTable::new();
+fn parse_header(buffer: Vec<u8>) -> Result<SectionTable, Error> {
+    let mut table = SectionTable::new();
     let entry_count = HEADER_ENTRIES * 24;
 
     let header = &buffer[0..entry_count];
@@ -75,23 +140,23 @@ fn parse_header(buffer: Vec<u8>) -> Result<DataTable, Error> {
 
         // Each entry must be exactly 24 bytes
         if chunk.len() == 24 {
-            let entry = Entry {
-                descriptor: [chunk[0], chunk[1], chunk[2], chunk[3]],
-                body: [
+            let section_name: String = [chunk[0], chunk[1], chunk[2], chunk[3]]
+                .iter()
+                .map(|&c| c as char)
+                .collect();
+
+            table
+                .get_mut(&section_name)
+                .unwrap_or_else(|| panic!("Error: section header {section_name} doesn't exist.",))
+                .set_value([
                     i32::from_le_bytes([chunk[4], chunk[5], chunk[6], chunk[7]]),
                     i32::from_le_bytes([chunk[8], chunk[9], chunk[10], chunk[11]]),
                     i32::from_le_bytes([chunk[12], chunk[13], chunk[14], chunk[15]]),
                     i32::from_le_bytes([chunk[16], chunk[17], chunk[18], chunk[19]]),
                     i32::from_le_bytes([chunk[20], chunk[21], chunk[22], chunk[23]]),
-                ],
-            };
-
-            let descriptor_string: String = entry.descriptor.iter().map(|&c| c as char).collect();
+                ]);
 
             // println!("{index}, {:?}, {:?}", descriptor_string, entry.body);
-
-            table.raw_entries[index] = entry;
-            table.table.insert(descriptor_string, entry.body);
         } else {
             return Err(Error::new(
                 ErrorKind::InvalidData,
